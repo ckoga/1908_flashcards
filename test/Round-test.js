0bb793e845338f31d96dff5669bb8f554f4f7809
Colin Turing <colin.koga@gmail.com>
2019-09-30 16:55:27 -0600

    Finish set up all files are correctly talking to each other with basic tests set up

--- test/Round-test.js
+++ test/Round-test.js
@@ -4,4 +4,17 @@ const expect = chai.expect;
 const Round = require('../src/Round');
 
 
-describe('Round')
+describe('Round', () => {
+  
+  it('should be a function', () => {
+    let round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', () => {
+    let round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  
+})
