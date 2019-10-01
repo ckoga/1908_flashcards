3a437f63013e50f324c278a0528ed99273cd617d
Colin Turing <colin.koga@gmail.com>
2019-10-01 08:29:09 -0600

    Add method and test that checks how many cards the Deck has

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -21,6 +21,15 @@ describe('Deck', () => {
     let deck = new Deck([card1, card2, card3])
 
     expect(deck.cards.length).to.equal(3);
-  })
+  });
+
+  it('should know how many cards are in the deck',() => {
+    let card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    let card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    let card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    let deck = new Deck([card1, card2, card3])
+
+    expect(deck.countCards()).to.equal(3)
+  });
   
 })
