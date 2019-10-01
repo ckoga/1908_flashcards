0ad766de80ddcf138ff3a7503613424c62b783f4
Colin Turing <colin.koga@gmail.com>
2019-09-30 18:46:31 -0600

    Add parameters to Card and Turn instances

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,17 +5,17 @@ const Turn = require('../src/Turn');
 
 describe('Turn', () => {
 
-  let turn = new Turn();
-  let card = new Card();
+  let card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  let turn = new Turn('object', card);
   
 
   it('should be a function', () => {
-    let turn = new Turn();
+    let turn = new Turn('object', card);
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', () => {
-    let turn = new Turn();
+    let turn = new Turn('object', card);
     expect(turn).to.be.an.instanceof(Turn);
   });
 
